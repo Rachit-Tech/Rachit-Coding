c3a3390ba01becb9ca8acc6566dbe4146374a38d
Rachit-Tech <rachitrathi99@gmail.com>
2022-01-22 16:10:29 +0530

    Valtriaz

--- Python/ObjectOrientedProg.py
+++ Python/ObjectOrientedProg.py
@@ -1,4 +1,4 @@
-#Classes are like scripatable objects
+# Classes are like scripatable objects
 class Number:
     def  sum(self):
         return self.a +self.b
@@ -8,3 +8,12 @@ num.a=12
 num.b=34
 s=num.sum()
 print(s)
+
+PouchPosition="The pouch is kept in the bag."
+class FindingPouch:
+    def findpouch(self):
+        return PouchPosition
+
+PouchLocation=FindingPouch()
+p=PouchLocation.findpouch()
+print(p)
