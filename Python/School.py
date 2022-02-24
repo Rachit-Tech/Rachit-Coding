ef8c3a747cd6004518304e527f5e967c9f1963ef
Rachit-Tech <rachitrathi99@gmail.com>
2022-02-24 18:11:08 +0530

    Valtriaz

---
+++ Python/School.py
@@ -0,0 +1,12 @@
+from typing import List
+from unicodedata import name
+from markupsafe import string
+
+
+class Details:
+    def name(self):
+        return 
+
+List1=Details()
+Details.name()
+print (List1, \n,"hello")
