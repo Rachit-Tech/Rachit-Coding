96ad3103e0956a00eb7815eb460aa38753c1e60e
Rachit-Tech <rachitrathi99@gmail.com>
2022-02-24 19:08:34 +0530

    Valtriaz

--- Python/School.py
+++ Python/School.py
@@ -1,6 +1,3 @@
-from msilib.schema import Class
-
-
 class Details:
     def details(self,name=(input("Name:")),age=(input("Age:")),Class=(input("Class:")),school=(input("School Name:"))):
         return print(" Name:",name,"\n","Age:",age,"\n","Class:",Class,"\n","School Name:",school)
