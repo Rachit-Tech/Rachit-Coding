e35bb489939bb1beaa75056d73e582800b1460c8
rachitrathi99@gmail.com <rachitrathi99@gmail.com>
2022-03-09 23:51:36 +0530

    Valtriaz

---
+++ Code Forces/Ladder/Lucky division.py
@@ -0,0 +1,61 @@
+n = float(input())
+if n==4:#1
+    print("YES")
+elif n==7:#2
+    print("YES")
+elif n==44:#3
+    print("YES")
+elif n==77:#4
+    print("YES")
+elif n==444:#5
+    print("YES")
+elif n==777:#6
+    print("YES")
+elif n==447:#7
+    print("YES")
+elif n==477:#8
+    print("YES")
+elif n==474:#9
+    print("YES")
+elif n==744:#10
+    print("YES")
+elif n==747:#11
+    print("YES")
+elif n==774:#12
+    print("YES")
+elif n==47:#13
+    print("YES")
+elif n==74:#14
+    print("YES")
+elif n%4==0:
+    print("YES")
+elif n%7==0:
+    print("YES")
+else:
+    print("NO")
+
+# m = list(n)
+# c = 0
+# # print(m)
+
+# for i in range(0,len(m)):
+#     if(m[i] == '4' or m[i] == '7'):
+#         c = c + 1
+
+# l=[4,7,44,47,74,77,444,447,474,477,744,747,774,777]
+# d=0
+# for j in range(0,len(l)):
+#     if n > 1[j]:
+#         if n % 1[j] == 0:
+#             d = d + 1
+
+# if d != 0 or c == len(m):
+#     print("YES")
+# else:
+#     print("NO")
+
+# a=[4,7,44,47,74,77,444,447,474,477,744,747,774,777]
+# if m in a:
+#     print("YES")
+# else:
+#     print("NO")
---
+++ Code Forces/Ladder/test.py
@@ -0,0 +1,5 @@
+a=input('enter value')
+b=(1,2,3,4)
+c=(5,6,7,8,9)
+if a in b:
+   print ('enter in code thats wanted')
